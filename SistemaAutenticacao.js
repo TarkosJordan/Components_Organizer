26e700bda5d44bb1c7fcf6f7ed769014fbbad84f
TarkosJordan <tarsis.orsi@gmail.com>
2021-08-02 20:52:06 -0300

    Adicionado teste para o parametro autenticavel verificando se o objeto passado possui o método autenticar implementado

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -5,6 +5,17 @@
  */
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha) 
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha) 
+        } else{
+            return false
+        }
+    }
+    /**
+     * Método testa se objeto passado como parametro possui em sua definição de classe um campo autenticar e se esse campo autenticar representa
+     * uma função dentro do contexto da classe que define esse objeto
+     */
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
